c9fb43e702905ac6d866e19f5a337b11aba1db8a
Naython <Naython@10.30.23.166>
2017-08-28 21:23:54 -0500

    Added a line of code to the main

--- src/firstgit/FirstGit.java
+++ src/firstgit/FirstGit.java
@@ -9,7 +9,7 @@ public class FirstGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hello Darkness my old friend");
     }
     
 }
